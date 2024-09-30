98244dc9fc136f1e3e7ef5ba3da90bc33ab2e707
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-09-30 12:47:07 +0000

    Set config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-
+st.set_page_config(page_title="Maykoll Rocha - Portifolio Pessoal",
+            page_icon=None ,
+            layout="wide",
+            initial_sidebar_state="expanded",
+        )
 
 st.write("Hello Word")
