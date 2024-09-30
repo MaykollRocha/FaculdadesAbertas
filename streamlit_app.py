a3824d5176cad457c0a4daa2d5034d2d27bf8d32
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-09-30 14:11:47 +0000

    Buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,6 @@ entra = st.number_input("Entra numero")
 st.write(entra)
 color  = st.color_picker('Pick a color',)
 st.write(color)
-st.set_page_config(bg=color)
 
 data = st.date_input("Entre com o dia")
 st.write(data)
